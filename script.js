4ca442fb6234e7c405d0df48f69d6abc271b3af0
kinasaiii <ainseriiness@gmail.com>
2026-07-17 16:45:57 +0100

    project container hover fix

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const frenchText = {
     "aboutme-paragraph": "Je suis étudiante en informatique à l’Université de Béjaïa, très passionnée par l’apprentissage, la programmation et la réalisation de projets en développement web. J’aime explorer de nouveaux outils et concepts afin de progresser en tant que développeuse, et j’apprécie particulièrement transformer des idées en sites web conviviaux et performants, tout en améliorant continuellement mes compétences. Je crois fortement à l’apprentissage par la pratique, et la majorité de mes progrès provient de l’expérimentation et de la création de projets.<br><br> Je maîtrise le langage C et l’algorithmique, ce qui me donne une base solide en résolution de problèmes, structures de données et raisonnement logique. En parallèle de mon parcours technique, je suis également entrepreneure avec Arvea, une expérience qui m’a permis de développer des compétences en communication, créativité et gestion d’activité. J’ai aussi une bonne compréhension du design UI/UX, me permettant de concevoir des interfaces à la fois fonctionnelles, intuitives et esthétiques.<br><br> Mon objectif est de devenir une développeuse logicielle compétente, de contribuer à des projets à fort impact et de continuer à apprendre et évoluer dans le domaine, en combinant expertise technique, créativité et esprit entrepreneurial.",
     "skill-title": "Mes Compétences",
     "project-title": "Mes Projets",
-    "project-link": "Voir Plus",
+    "project-link": "Voir Plus →",
     "jeu-mot": "Jeu de Mot en C",
     "jeu-mot-desc": "Un Jeu de génération de mots en C, basé sur l’algorithmique et la résolution de problèmes, où les joueurs forment des mots à partir de lettres aléatoires validées par un dictionnaire.",
     "kinasai-links": "Social Links",
@@ -78,7 +78,7 @@ function switchLang () {
 
     // translate "see more" buttons (class-based)
     document.querySelectorAll(".project-link").forEach(btn => {
-        btn.innerText = currentLang === "fr" ? "Voir Plus" : "See More";
+        btn.innerText = currentLang === "fr" ? "Voir Plus →" : "See More →";
     });
 
     document.querySelectorAll(".tooltip-label").forEach(btn => {
